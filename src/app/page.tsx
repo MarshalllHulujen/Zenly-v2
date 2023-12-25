2ac99876d2b9fba414cc4caf15243201e6f34257
Хүлүгэн Баасанжав <HP236710@G304-CC0041.local>
2023-12-25 19:23:49 +0800

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,36 @@
-import Image from "next/image";
-import { ChatList } from "./components/chat/ChatList";
-import { ChatInput } from "./components/chat/ChatInput";
+// "use client";
 
-export default function Home() {
-  return (
-    <div>
-      <ChatList />
-      <ChatInput />
-    </div>
-  );
-}
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useUser } from "./hooks/userUser";
+// // import { User } from "penly/types/Users";
+// import { fetcher } from "./utils/fetcher";
+// import useSWR from "swr";
+
+// export default function Home() {
+//   const { user } = useUser();
+//   const { data: usersData, isLoading: usersLoading, error: usersError } = useSWR("/api/users", fetcher);
+//   console.log("usersData:", usersData);
+
+//   if (!user) return <div>loading...</div>;
+
+//   return (
+//     <div className="flex w-full h-screen flex-col py-12 px-9">
+//       <div className="flex gap-4 items-center">
+//         <Image src={user.imageUrl} alt={user.name} width={36} height={36} className="rounded-full" />
+//         <p className="font-bold">{user.name}</p>
+//       </div>
+//       <div className="border-t border-b py-4 my-4 border-white/30">
+//         {usersData?.map((user: User) => (
+//           <Link href={`/new?to=${user.id}`} className="flex gap-6 items-center" key={user.id}>
+//             <Image src={user.imageUrl} alt={user.name} width={48} height={48} className="rounded-full" />
+//             <div>
+//               <p>{user.name}</p>
+//               <p>Start conversation...</p>
+//             </div>
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
--- src/app/service/conversation-service.ts
+++ src/app/service/conversation-service.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 import { mongoApiRequest } from "../utils/mongoApiRequest";
 import { Conversation } from "../types/Conversation";
 import { SimpleResponse } from "../types";
-import { Prisma } from "@prisma/client";
-import { prisma } from "../utils/prisma";
 // api-url https://ap-south-1.aws.data.mongodb-api.com/app/data-diolp/endpoint/data/v1
 // api-key = YkPHUTHBmy5tIRvdnOsb4FruNu7I8MfKaOpIoRACElDIGW1HUarjnC1cDsCzXuiu
 
@@ -38,12 +36,24 @@ export const getConversation = async (
 };
 
 export const createConversation = async (
-  input: Prisma.ConversationCreateInput
-) => {
-  try {
-    const result = await prisma.conversation.create({ data: input });
-    return { response : result }
-  } catch (error) {
-    return error
+  members: string[]
+): Promise<Conversation | null> => {
+  const newConversation = {
+    _id: nanoid(),
+    members,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  const { response, error } = await mongoApiRequest(
+    "insertOne",
+    "conversations",
+    {
+      document: newConversation,
+    }
+  );
+  if (error) {
+    console.error(error);
+    return null;
   }
+  return newConversation;
 };
